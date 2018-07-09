52ba4c144d2de936a3bdd03fad8e3f412d4b4752
Le Zhang <ZhangL@duerkopp-adler.com>
2018-07-09 17:57:48 +0200

    All life circle

---
+++ src/app/life/life.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, SimpleChanges, Input } from '@angular/core';
+
+let logIndex: number = 1;
+
+@Component({
+  selector: 'app-life',
+  templateUrl: './life.component.html',
+  styleUrls: ['./life.component.css']
+})
+export class LifeComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  @Input()
+  name: String;
+
+  logIt(msg: string) {
+    console.log(`#${logIndex++} ${msg}`);
+  }
+
+  constructor() {
+    this.logIt("name属性在constructor里的值是：" + name);
+  }
+
+  ngOnInit(): void {
+    this.logIt("ngOnInit");
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    let name = changes['name'].currentValue;
+    this.logIt("name属性在ngOnChanges里的值是：" + name);
+  }
+
+  ngDoCheck(): void {
+    this.logIt("ngDoCheck");
+  }
+
+  ngAfterContentInit(): void {
+    this.logIt("ngAfterContentInit");
+  }
+
+  ngAfterContentChecked(): void {
+    this.logIt("ngAfterContentChecked");
+  }
+
+  ngAfterViewInit(): void {
+    this.logIt("ngAfterViewInit");
+  }
+
+  ngAfterViewChecked(): void {
+    this.logIt("ngAfterViewChecked");
+  }
+
+  ngOnDestroy(): void {
+    this.logIt("ngOnDestroy");
+  }
+
+}
