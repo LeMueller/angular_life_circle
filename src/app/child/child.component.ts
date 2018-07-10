9fb89902b1fa3726f6fbf64656bf84b0ada6b5b0
Le Zhang <ZhangL@duerkopp-adler.com>
2018-07-10 10:43:15 +0200

    DoCheck called by everything, use if to select useful changes!

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit, OnChanges {
+export class ChildComponent implements OnInit, OnChanges, DoCheck {
 
   @Input()
   greeting: string;
@@ -13,7 +13,16 @@ export class ChildComponent implements OnInit, OnChanges {
   @Input()
   user: {name:string};
 
+  @Input()
+  stock: {name:string};
+
   message:string = "初始化信息";
+
+  oldUserkName: string;
+
+  changeDtected: boolean;
+
+  changeCount:number = 0;
 
   constructor() { }
 
@@ -23,6 +32,24 @@ export class ChildComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     console.log(JSON.stringify(changes, null, 2));
     
+  }
+
+  ngDoCheck(): void {
+    if (this.user.name !== this.oldUserkName) {
+      this.changeDtected = true;
+      console.log("DoCheck: user.name 从" +this.oldUserkName + " 变为" + this.user.name);
+      this.oldUserkName = this.user.name;
+    }
+
+    if (this.changeDtected) {
+      this.changeCount = 0;
+    } else {
+      this.changeCount ++;
+      console.log("DoCheck: user.name 没变化时， ngDoCheck方法被调用了" + this.changeCount + "次");
+
+    }
+
+    this.changeDtected = false;
   }
 
 }
